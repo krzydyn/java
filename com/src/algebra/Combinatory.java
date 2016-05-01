a8dca754fdef306900d3c6b16a5256ea1062c05d
Krzysztof Dynowski <krzysztof.dynowski@xyo.net>
2016-05-01 15:20:47 +0200

    nextPermutation works on generic List

--- com/src/algebra/Combinatory.java
+++ com/src/algebra/Combinatory.java
@@ -1,9 +1,22 @@
 package algebra;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Combinatory {
-	static public void nextPermutation(List<Object> list) {
+	
+	static public <T extends Comparable<T>> boolean nextPermutation(List<T> a) {
+        int i = a.size() - 2;
+        
+        while (i >= 0 && a.get(i).compareTo(a.get(i + 1)) >= 0) i--;
 
+        if (i < 0) return false;
+
+        int j = a.size() - 1;
+        while (a.get(i).compareTo(a.get(j)) >= 0) j--;
+
+        Collections.swap(a, i, j);
+        Collections.reverse(a.subList(i + 1, a.size()));
+        return true;
 	}
 }
