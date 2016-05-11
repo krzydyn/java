3ed315b81e89bd2d2c9d7fdc14203d4939323328
Krzysztof Dynowski <k.dynowski@samsung.com>
2016-05-11 14:54:23 +0200

    simple(pixel) implementation of floodFill

--- com/src/algebra/Planes.java
+++ com/src/algebra/Planes.java
@@ -3,7 +3,6 @@ package algebra;
 import java.awt.Dimension;
 import java.util.ArrayList;
 
-
 public class Planes {
 	public static interface Raster {
 		public int getPixel(int x,int y);
@@ -64,4 +63,30 @@ public class Planes {
 			}
 		}
 	}
+
+	static class Coord {
+		public Coord(int x,int y) {
+			this.x=x; this.y=y;
+		}
+		int x,y;
+	}
+	static void floodFill_simple(Raster t, int x,int y, int dval) {
+		int sval=t.getPixel(x,y);
+		if (sval==dval) return ;
+
+		Dimension d = t.getSize();
+		ArrayList<Coord> q = new ArrayList<Coord>();
+		q.add(new Coord(x, y));
+
+		while (!q.isEmpty()) {
+			Coord c=q.remove(q.size()-1);
+			x=c.x;y=c.y;
+			t.setPixel(x, y, dval);
+			if (x>0 && t.getPixel(x-1,y)==1) q.add(new Coord(x-1, y));
+			if (x+1<d.width && t.getPixel(x+1,y)==1) q.add(new Coord(x+1, y));
+			if (y>0 && t.getPixel(x,y-1)==1) q.add(new Coord(x, y-1));
+			if (y+1<d.height && t.getPixel(x,y+1)==1) q.add(new Coord(x, y+1));
+		}
+	}
+
 }
