0e3d1349cea509b7ccd0483d5de36e8b75d462ac
Krzysztof Dynowski <k.dynowski@samsung.com>
2016-06-13 09:21:37 +0200

    rename Matrix2I

--- com/src/algebra/MatrixI.java
+++ com/src/algebra/MatrixI.java
@@ -1,29 +1,29 @@
 package algebra;
 
-public class Matrix2I {
+public class MatrixI {
 	private final int w,h;
 	private final int[] v;
 	private int M=1; // modulo
 
-	public Matrix2I(int w,int h) {
+	public MatrixI(int w,int h) {
 		this.w=w; this.h=h;
 		v = new int[w*h];
 	}
 	public int get(int x,int y) {return v[y*w+x];}
 	public int set(int x,int y,int v) {return this.v[y*w+x]=v;}
 
-	public Matrix2I zero() {
+	public MatrixI zero() {
 		for(int i = 0 ; i < v.length ; i++)  v[i]=0;
 		return this;
 	}
-	public Matrix2I unit() {
+	public MatrixI unit() {
 		for(int i = 0 ; i < v.length ; i++)  v[i]=0;
 		int hw = Math.min(w, h);
 		for(int i = 0 ; i < hw; i ++) set(i,i,1);
 		return this;
 	}
 
-	public Matrix2I mul(Matrix2I m) {
+	public MatrixI mul(MatrixI m) {
 		int[] tv = new int[w*h];
 		for(int i = 0 ; i < tv.length ; i++) tv[i]=0;
 
@@ -40,9 +40,9 @@ public class Matrix2I {
 		return this;
 	}
 
-	public Matrix2I pow(int n) {
+	public MatrixI pow(int n) {
 		if (n==1) return this;
-		Matrix2I t=new Matrix2I(w, h).unit();
+		MatrixI t=new MatrixI(w, h).unit();
 		while(n!=0) {
 	        if ((n&1)!=0) t.mul(this);
 	        mul(this);
