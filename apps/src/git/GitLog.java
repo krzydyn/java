5708db534745262522ba671836d9bf3ad55e63a2
Krzysztof Dynowski <k.dynowski@samsung.com>
2016-09-05 09:46:59 +0200

    gitlog --all test

--- apps/src/git/GitLog.java
+++ apps/src/git/GitLog.java
@@ -26,15 +26,18 @@ import sys.Log;
 
 public class GitLog {
 	static final String[] ArrayOfString0 = new String[0];
-	//static GitRepo repo = new GitRepo("~/sec-os/secos");
-	static GitRepo repo = new GitRepo("~/tmp/linux");
 	//static GitRepo repo = new GitRepo("~/tmp/nuclear-js");
 	//static String branch = "origin/master";
-	static String branch = "2cde51fbd0f3"; //linux octopus commit
+
+	//static GitRepo repo = new GitRepo("~/tmp/linux");
+	//static String branch = "2cde51fbd0f3"; //linux octopus commit
 	//static String branch ="7c4c62a";
-	static int limit = 8000;
+
+	static GitRepo repo = new GitRepo("~/sec-os/secos");
+	static String branch = "--all";
 	//static String branch = "origin/devel/k.debski/openssl-20160801";
 	//static String branch = "origin/devel/anchit/gatekeeper";
+	static int limit = 8000;
 
 	public static void main(String[] args) {
 		GitGraph graph = new GitGraph(repo);
