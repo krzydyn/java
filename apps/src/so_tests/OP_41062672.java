b9321ec6e6bed78fd6e4f3ba62287a6005945ea9
krzydyn <krzydyn@gmail.com>
2016-12-13 11:01:46 +0100

    Stackoverflow answ

---
+++ apps/src/so_tests/OP_41062672.java
@@ -0,0 +1,40 @@
+package so_tests;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class OP_41062672 extends JPanel {
+
+	public OP_41062672() {
+		//setBackground(new Color(255, 0, 0, 255));
+		setBackground(new Color(0,true));
+		setPreferredSize(new Dimension(400,400));
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.setColor(getBackground());
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, getWidth()-1, getHeight()-1);
+		g.setColor(Color.RED);
+		g.drawOval(0, 0, getWidth()-1, getHeight()-1);
+	}
+
+	public static void main(String[] args) {
+		JFrame f=new JFrame();
+		f.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+		f.setUndecorated(true);
+		f.setBackground(new Color(0,true));
+		//f.setOpacity(1f);
+		f.setContentPane(new OP_41062672());
+		f.pack();
+		f.setVisible(true);
+	}
+
+}
---
+++ apps/src/so_tests/OP_41096652.java
@@ -0,0 +1,82 @@
+package so_tests;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class OP_41096652 {
+	@SuppressWarnings("serial")
+	static public class IconPanel extends JPanel {
+		private ImageIcon characterIntro;
+		private ImageIcon characterIdle;
+		private ImageIcon characterAttack;
+		private ImageIcon characterJump;
+		private ImageIcon[] ico;
+
+		private JLabel lbl = new JLabel();
+		private boolean running=true;
+
+		public IconPanel() {
+			super(new GridLayout(1,0));
+			try { loadIcons(this); } catch (IOException e) {e.printStackTrace();}
+			lbl.setPreferredSize(new Dimension(50, 50));
+			add(lbl);
+
+			new Thread(){
+				@Override
+				public void run() {
+					int[] idxrep = {0,1,2,1,3};
+					int[] dlyrep = {5,3,6,5,3};
+
+					int idx=0;
+					while (running) {
+						lbl.setIcon(ico[idxrep[idx]]);
+						try {Thread.sleep(dlyrep[idx]*100);}catch(Exception e){}
+
+						++idx;
+						if (idx == idxrep.length) idx=1;
+					}
+				}
+			}.start();
+		}
+	}
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+		final IconPanel p = new IconPanel();
+		f.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				super.windowClosed(e);
+				p.running=false;
+			}
+		});
+		f.setContentPane(p);
+		f.pack();
+		f.setVisible(true);
+	}
+
+	private static ImageIcon readIcon(String file) throws IOException {
+		Image i = ImageIO.read(new File(file));
+		return new ImageIcon(i.getScaledInstance(50, 50, BufferedImage.TYPE_INT_RGB));
+	}
+	private static void loadIcons(IconPanel p) throws IOException {
+		p.characterIntro = readIcon("res/skul1.png");
+		p.characterIdle = readIcon("res/skul2.png");
+		p.characterAttack = readIcon("res/skul3.png");
+		p.characterJump = readIcon("res/skul4.png");
+		p.ico = new ImageIcon[]{p.characterIntro, p.characterIdle, p.characterAttack, p.characterJump};
+	}
+}
