656bb247fee3227f01304f04cb65c9fa07eeb47a
krzydyn <krzydyn@gmail.com>
2017-03-14 08:10:12 +0100

    update activity mon

--- apps/src/activity/ActivityMon.java
+++ apps/src/activity/ActivityMon.java
@@ -3,6 +3,9 @@ package activity;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 import sys.Env;
 import sys.Log;
 import sys.XThread;
@@ -60,29 +63,66 @@ done
 		}
 	}
 	static class WindowsMon implements ActivityMonitor {
+		/*
+		public interface XLib extends StdCallLibrary {
+		    XLib INSTANCE = (XLib) Native.loadLibrary("XLib", Psapi.class);
 
+		    int XGetInputFocus(X11.Display display, X11.Window focus_return, Pointer revert_to_return);
+		}*/
 		@Override
 		public void run() throws Exception {
 		}
-
+	}
+	static class MacosMon implements ActivityMonitor {
+		final static String script="tell application \"System Events\"\n" +
+                "name of application processes whose frontmost is true\n" +
+                "end\n";
+		final static String script2="global frontApp, frontAppName, windowTitle\n"
+			+ "set windowTitle to \"\"\n"
+			+ "tell application \"System Events\"\n"
+				+ "set frontApp to first application process whose frontmost is true\n"
+				+ "set frontAppName name of frontApp\n"
+				//+ "tell process frontApp\n"
+                	+ "tell (first window whose value of attribute \"AXMain\" is true)\n"
+                	+ "set windowTitle to value of attribute \"AXTitle\"\n"
+                	+ "end tell\n"
+                //+ "end tell\n"
+            + "end\n"
+            + "return {frontAppName, windowTitle}\n";
+		final static String script3 = "global appProc,appName\n"
+			+ "tell application \"System Events\"\n"
+		        + "set appProc to first application process whose frontmost is true\n"
+		        + "set appName to name of appProc\n"
+		    + "end\n"
+		    + "return {appName,appProc}\n";
+		@Override
+		public void run() throws Exception {
+			ScriptEngine se = new ScriptEngineManager().getEngineByName("AppleScript");
+			String result=Text.join("\n",se.eval(script3));
+	        System.out.println(result);
+		}
 	}
 
 	static ActivityMonitor monitor = null;
 	public static void main(String[] args) {
-		if ("Linux".equalsIgnoreCase(Env.osName())) {
+		String osname = Env.osName();
+		if ("Linux".equalsIgnoreCase(osname)) {
 			monitor = new LinuxMon();
 		}
-		else if ("Windows".equalsIgnoreCase(Env.osName())) {
+		else if ("Mac OS X".equals(osname)) {
+			monitor = new MacosMon();
+		}
+		else if ("Windows".equalsIgnoreCase(osname)) {
 			monitor = new WindowsMon();
 		}
 		else {
-			Log.error("not supported os: %s", Env.osName());
+			Log.error("not supported os: '%s' %s", osname, Text.vis(osname));
 			return ;
 		}
 		try {
 			while (true) {
 				monitor.run();
-				XThread.sleep(1000);
+				XThread.sleep(5000);
 			}
 		}
 		catch (Throwable e) {
