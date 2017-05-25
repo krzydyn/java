874c77afa000d70b7f9a0b7b38f29cfa390e5311
krzydyn <krzydyn@gmail.com>
2017-05-25 13:51:04 +0200

    update word sql table

--- apps/res/espdb/word.sql
+++ apps/res/espdb/word.sql
@@ -1,1 +1,7 @@
-CREATE TABLE word (id INTEGER PRIMARY KEY,word VARCHAR,descr TEXT,UNIQUE(word));
+-- word is a one word
+-- attrs is  set of word attributes
+--		verb (Indefinido,Participio,Gerundio,Other)
+--		noun (Gender(m,f),Number(s,p),Pronoun(zaimek),Article(rodzajnik))
+--		adjective ()
+--		adverb ()
+CREATE TABLE word (id INTEGER PRIMARY KEY,word VARCHAR(255),attrs varchar(10),UNIQUE(word));
